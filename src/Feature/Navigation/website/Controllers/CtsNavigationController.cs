150aad679efd1b469b10bbf0d7189b43c8a0e977
Gopi S V <175238@cognizant.com>
2025-05-19 09:41:23 +0530

    HEader navigation basics

---
+++ src/Feature/Navigation/website/Controllers/CtsNavigationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CognizantPlc.Feature.Navigation.Controllers
+{
+    public class CtsNavigationController : Controller
+    {
+        // GET: CtsNavigation
+        public ActionResult Header()
+        {
+            return View();
+        }
+    }
+}
